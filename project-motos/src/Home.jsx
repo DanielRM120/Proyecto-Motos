366b1f6015d8b9f399a862c05cd9a3f74f6f3572
DanielRM120 <daaleroma@gmail.com>
2022-11-05 23:17:02 -0500

    Home

--- project-motos/src/Home.jsx
+++ project-motos/src/Home.jsx
@@ -2,10 +2,86 @@
 
 const Home = () => {
     return(
-        <>
-            <h1>Home - Proyecto Grupo G32</h1>
-            
-        </>
+        <><>
+            <section id="hero">
+                <h1>Pide los mejores<br />Técnicos</h1>
+                <form>
+                    <button>REGISTRATE YA!</button>
+                </form>
+            </section>
+            <section id="hero2">
+                <div class="container">
+                    <div class="img-container"></div>
+                    <div class="texto">
+                        <h2>Somos <span class="color-titulo-hero2"> Motorbike upkeep!</span></h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
+                            possimus temporibus minus nostrum iure pariatur eligendi vitae
+                            labore illum qui autem voluptatum asperiores magni facilis,
+                            molestias veniam ipsam ut.
+                        </p>
+                    </div>
+                </div>
+            </section>
+
+            <section id="nuestros-beneficios">
+                <div class="container">
+                    <h2>Nuestros Planes</h2>
+                    <div class="beneficios">
+                        <div class="carta">
+                            <h3>Basico</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+                                enim nulla doloremque itaque aspernatur deleniti? Accusantium ex,
+                                consectetur sint praesentium ab obcaecati, repellendus nam dolorem
+                                placeat fugit eligendi blanditiis iusto.
+                            </p>
+                            <button>+ Info</button>
+                        </div>
+
+                        <div class="carta">
+                            <h3>Gold</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+                                enim nulla doloremque itaque aspernatur deleniti? Accusantium ex,
+                                consectetur sint praesentium ab obcaecati, repellendus nam dolorem
+                                placeat fugit eligendi blanditiis iusto.
+                            </p>
+                            <button>+ Info</button>
+                        </div>
+
+                        <div class="carta">
+                            <h3>Platinum</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+                                enim nulla doloremque itaque aspernatur deleniti? Accusantium ex,
+                                consectetur sint praesentium ab obcaecati, repellendus nam dolorem
+                                placeat fugit eligendi blanditiis iusto.
+                            </p>
+                            <button>+ Info</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id="caracteristicas">
+                <div class="container">
+                    <li>✔ Domicilio</li>
+                    <li>✔ Tecnicos Verificados</li>
+                    <li>✔ Certificacion</li>
+                </div>
+            </section>
+        </><footer>
+                <div class="container">
+                    <h2>Contáctanos</h2>
+                    <li>123-345-789</li>
+                    <br />
+                    <li>Carrera 13D 100-23 Bogota D.C</li>
+                    <br />
+                    <li>Lunes a Sábado de 7 AM a 5 PM</li>
+                    <br />
+                </div>
+            </footer></>
     )
 
 }
